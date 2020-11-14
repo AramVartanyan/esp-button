93034aa6b31d66503f7309171247bf5c68bbfa55
AVartanyan <42091996+AramVartanyan@users.noreply.github.com>
2020-11-14 22:18:54 +0200

    Separate ESP8266 and ESP32 settings

--- port.h
+++ port.h
@@ -2,14 +2,24 @@
 
 #include <stdint.h>
 
-#ifdef ESP_IDF
+#ifdef CONFIG_IDF_TARGET_ESP32
+
     #include <freertos/FreeRTOS.h>
     #include <freertos/semphr.h>
     #include <freertos/timers.h>
+
+#elif CONFIG_IDF_TARGET_ESP8266
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
+#include <freertos/timers.h>
+
 #else
+//ESP-OPEN-RTOS
     #include <FreeRTOS.h>
     #include <semphr.h>
     #include <timers.h>
+
 #endif
 
 void my_gpio_enable(uint8_t gpio, bool active_high);
