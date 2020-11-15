a9ea5616f20c3404945b9c4bfdd38e7a3efd821e
AVartanyan <42091996+AramVartanyan@users.noreply.github.com>
2020-11-15 16:37:33 +0200

    Add Toggle active state

--- toggle.h
+++ toggle.h
@@ -5,5 +5,5 @@
 
 typedef void (*toggle_callback_fn)(bool high, void* context);
 
-int toggle_create(uint8_t gpio_num, toggle_callback_fn callback, void* context);
+int toggle_create(uint8_t gpio_num, toggle_callback_fn callback, bool active_high, void* context);
 void toggle_delete(uint8_t gpio_num);
